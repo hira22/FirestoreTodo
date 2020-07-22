3dd7d5be7ac1ded5ffff85b66ea8dcded209db9c
hira22 <masayuki.hiraoka@ietty.co.jp>
2020-07-22 20:44:47 +0900

    Initial Commit

---
+++ FirestoreTodo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FirestoreTodo
+//
+//  Created by hiraoka on 2020/07/22.
+//  Copyright © 2020 hiraoka. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
