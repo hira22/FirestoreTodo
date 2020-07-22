ff3851b70da963e76b23d3bb9a6ff4c7db7d3f9d
hira22 <masayuki.hiraoka@ietty.co.jp>
2020-07-22 21:55:34 +0900

    first commit

--- FirestoreTodo/View/TaskListView.swift
+++ FirestoreTodo/View/TaskListView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  TaskListView.swift
 //  FirestoreTodo
 //
 //  Created by hiraoka on 2020/07/22.
@@ -8,7 +8,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct TaskListView: View {
     var body: some View {
         Text("Hello, World!")
     }
@@ -16,6 +16,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        TaskListView()
     }
 }
---
+++ FirestoreTodo/ViewModel/TaskListViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  TaskListViewModel.swift
+//  FirestoreTodo
+//
+//  Created by hiraoka on 2020/07/22.
+//  Copyright © 2020 hiraoka. All rights reserved.
+//
+
+import Foundation
